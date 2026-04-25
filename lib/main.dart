ce169747779c71497bd816ddced0391fb95c9ec7
iamthejafar <jafarjalali128@gmail.com>
2026-04-25 13:28:07 +0200

    fix: routing issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@ import 'package:chess/src/core/app.dart';
 import 'package:chess/src/features/landing/repositories/auth_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   final authRepository = AuthRepository();
   await authRepository.initialize();
   await GetStorage.init();
+  usePathUrlStrategy();
   runApp(App(authRepository: authRepository));
 }
